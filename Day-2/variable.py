476f4d8d85c1d7782545d37d6bd5629b3d39976d
Piyush Singh <piyusshthakur5@gmail.com>
2024-09-24 11:48:34 +0530

    update

--- Day-2/variable.py
+++ Day-2/variable.py
@@ -14,3 +14,25 @@ is_true=True
 is_light_on=False
 name,age,city='arush', 18 ,'Kishtwar'
 print(name,age,city)
+
+print(type(age))
+print(len(first_name))
+if(len(first_name)>=len(last_name)):
+    print("yoyoho")
+
+# 4
+num_one=5
+num_two=4
+
+total = num_one + num_two
+diff = num_one - num_two
+product = num_one * num_two
+division = num_one / num_two
+remainder = num_two % num_one
+
+
+print("Total:", total)
+print("Difference:", diff)
+print("Product:", product)
+print("Division:", division)
+print("Remainder:", remainder)

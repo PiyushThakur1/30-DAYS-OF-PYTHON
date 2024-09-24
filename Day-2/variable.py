69125c9a1b29c3c5cbe0724ad2e10165c44b80c0
Piyush Singh <piyusshthakur5@gmail.com>
2024-09-24 11:20:40 +0530

    update

---
+++ Day-2/variable.py
@@ -0,0 +1,16 @@
+# Day 2: 30 Days of python programming
+first_name='Anush'
+last_name='Jain'
+
+fullname=first_name +' '+last_name
+print(fullname)
+
+country='India'
+City='Jammu'
+age=20
+year=2024
+is_married='single'
+is_true=True
+is_light_on=False
+name,age,city='arush', 18 ,'Kishtwar'
+print(name,age,city)

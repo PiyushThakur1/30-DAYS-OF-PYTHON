e437d9bbe6665dce3c877dc3165ff3d6a648607a
Piyush Singh <piyusshthakur5@gmail.com>
2024-09-24 12:00:50 +0530

    update

---
+++ Day-2/built-in_input.py
@@ -0,0 +1,9 @@
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+country = input("Enter your country: ")
+age = int(input("Enter your age: "))
+
+print("First Name:", first_name)
+print("Last Name:", last_name)
+print("Country:", country)
+print("Age:", age)
